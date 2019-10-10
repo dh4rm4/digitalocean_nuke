3c46e7d0799deb6f7307dbd15b334cf6ce395e0e
dh4rm4 <dh4rm4@protonmail.com>
2019-10-10 19:32:34 +0200

    clean code

--- src/control_center.py
+++ src/control_center.py
@@ -29,9 +29,6 @@ class control_center:
             curr_class().launch_nuke()
             self._print_ok(f_name)
         except Exception:
-            err_msg = 'Exception occured during "{0}" method ' \
-                      'execution'.format(f_name)
-            logging.exception(err_msg)
             self._print_ko(f_name)
 
     def _print_ok(self, func_name: str) -> NoReturn:
@@ -39,9 +36,14 @@ class control_center:
         print('\033[92mOK\033[0m')
 
     def _print_ko(self, func_name: str) -> NoReturn:
+        self._log_error(func_name)
         print(func_name, end=':\t')
         print('\033[91mKO\033[0m')
 
+    def _log_error(self, f_name: str):
+        err_msg = 'Exception occured during "{0}" method ' \
+                  'execution'.format(f_name)
+        logging.exception(err_msg)
 
 if __name__ in '__main__':
     control_center().start_end_of_time()
